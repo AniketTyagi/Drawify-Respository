a299d519b8c8316fbd4f5bf0985eeb70cb635944
WyattPH224 <wyatt.harris224@gmail.com>
2023-03-09 15:13:56 -0800

    Functional website files, add to glitch to get working

---
+++ app/firebaseConfig.js
@@ -0,0 +1,17 @@
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCFmSpjRO2HqWqG1VN10PC7DFyZptjsAiY",
+  authDomain: "drawify-c2b73.firebaseapp.com",
+  projectId: "drawify-c2b73",
+  storageBucket: "drawify-c2b73.appspot.com",
+  messagingSenderId: "901440284138",
+  appId: "1:901440284138:web:58882aae7b6c2ac837fe93",
+  measurementId: "G-5MR6X7Y47J"
+};
+// const analytics = getAnalytics(app);
